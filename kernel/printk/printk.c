90edd706655de4471d8a06d0600ff41fd885d322
Yaroslav Furman <yaro330@gmail.com>
2019-07-13 15:18:13 +0300

    printk: use on-stack allocations for kernel log
    These allocationsare just 1kb in size, using kmalloc is not
    worth it for them. This should speed up printing of kernel log
    when uptime gets very long.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: atndko <z1281552865@gmail.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1334,13 +1334,11 @@ static size_t msg_print_text(const struct printk_log *msg, bool syslog, char *bu
 
 static int syslog_print(char __user *buf, int size)
 {
-	char *text;
+	char text[LOG_LINE_MAX + PREFIX_MAX];
 	struct printk_log *msg;
 	int len = 0;
 
-	text = kmalloc(LOG_LINE_MAX + PREFIX_MAX, GFP_KERNEL);
-	if (!text)
-		return -ENOMEM;
+	memset(&text, 0, LOG_LINE_MAX + PREFIX_MAX);
 
 	while (size > 0) {
 		size_t n;
@@ -1389,21 +1387,18 @@ static int syslog_print(char __user *buf, int size)
 		buf += n;
 	}
 
-	kfree(text);
 	return len;
 }
 
 static int syslog_print_all(char __user *buf, int size, bool clear)
 {
-	char *text;
+	char text[LOG_LINE_MAX + PREFIX_MAX];
 	int len = 0;
 	u64 next_seq;
 	u64 seq;
 	u32 idx;
 
-	text = kmalloc(LOG_LINE_MAX + PREFIX_MAX, GFP_KERNEL);
-	if (!text)
-		return -ENOMEM;
+	memset(&text, 0, LOG_LINE_MAX + PREFIX_MAX);
 
 	logbuf_lock_irq();
 	/*
@@ -1468,7 +1463,6 @@ static int syslog_print_all(char __user *buf, int size, bool clear)
 	}
 	logbuf_unlock_irq();
 
-	kfree(text);
 	return len;
 }
 
